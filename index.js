b5ab5ee7223385f6455837fa94ffe040b6997f44
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 20:04:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge+40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge+20;
 
     if (false /**
                * Think about it -- what's happening here?
@@ -61,12 +61,13 @@ function createRock(x) {
   var top = 0
 
   rock.style.top = top
+  //GAME.appendChild(rock)
 
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-
+    
 
   /**
    * This function moves the rock. (2 pixels at a time
@@ -79,6 +80,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+     
 
     /**
      * Otherwise, if the rock hasn't reached the bottom of
@@ -92,7 +94,8 @@ function createRock(x) {
   }
 
   // We should kick of the animation of the rock around here
-
+  window.requestAnimationFrame();
+  
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
@@ -108,6 +111,9 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
+  clearInterval(gameInterval)
+  return window.alert('YOU LOSE!')
+  document.removeEventListener('keydown', moveDodger)
 }
 
 function moveDodger(e) {
@@ -119,6 +125,17 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   
+   document.addEventListener('keydown', function(key) {
+     if (e.which === LEFT_ARROW) {
+       e.stopPropagation();
+       moveDodgerLeft();
+     } else if (e.which === RIGHT_ARROW) {
+       e.preventDefault();
+       moveDodgerRight();
+     }
+   })
+   
 }
 
 function moveDodgerLeft() {
